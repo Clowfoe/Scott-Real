f2ca3797d6f48d812dbfeface235879316c36003
moonrocklullaby <67526823+moonrocklullaby@users.noreply.github.com>
2021-07-16 02:03:38 +1200

    gbhfxd

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -155,7 +155,7 @@ class TitleState extends MusicBeatState
 			FlxG.sound.music.fadeIn(4, 0, 0.7);
 		}
 
-		Conductor.changeBPM(102);
+		Conductor.changeBPM(112);
 		persistentUpdate = true;
 
 		var bg:FlxSprite = FlxGradient.createGradientFlxSprite(FlxG.width, FlxG.height, [FlxColor.BLACK, FlxColor.BLUE]);
